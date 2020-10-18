a830e4edec3a18541921497f778538b8a544a57b
mallikarjun <ext_smallikarjun@Falabella.cl>
2020-10-19 02:40:02 +0530

    Updates

---
+++ precache-manifest.9ccc34a84003aa2321d878b569528a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420904700cea75eb1e9039aba0906b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1720227db579e105f9e3",
+    "url": "/static/css/main.4b321f82.chunk.css"
+  },
+  {
+    "revision": "52ee9d463fd34adf31ad",
+    "url": "/static/js/2.1d604f2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1d604f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1720227db579e105f9e3",
+    "url": "/static/js/main.514801cd.chunk.js"
+  },
+  {
+    "revision": "a17915959ef6a8d53770",
+    "url": "/static/js/runtime-main.60ee0e28.js"
+  }
+]);
