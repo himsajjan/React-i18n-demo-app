66c1a11b852fb9dc4233862869e83a2eab63a3e5
mallikarjun <ext_smallikarjun@Falabella.cl>
2020-10-18 22:06:43 +0530

    inital setup

---
+++ src/components/Wrapper.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { IntlProvider } from "react-intl";
+import French from "../lang/fr.json";
+import Arabic from "../lang/ar.json";
+import English from "../lang/en.json";
+import German from "../lang/de.json";
+
+export const Context = React.createContext();
+
+const local = navigator.language.split(/[-_]/)[0];
+
+const supportedMessages = {
+  fr: French,
+  en: English,
+  ar: Arabic,
+  de: German
+};
+const language = navigator.language.split(/[-_]/)[0];
+
+const Wrapper = (props) => {
+  const [locale, setLocale] = useState(language);
+
+  const [selectedMessage, setMessages] = useState(supportedMessages[local]);
+
+  function selectLanguage(e) {
+    const newLocale = e.target.value;
+    setLocale(newLocale);
+    setMessages(supportedMessages[newLocale])
+  }
+
+  return (
+    <Context.Provider value={{ locale, selectLanguage }}>
+      <IntlProvider messages={selectedMessage} locale={locale}>
+        {props.children}
+      </IntlProvider>
+    </Context.Provider>
+  );
+};
+
+export default Wrapper;
